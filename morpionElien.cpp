0fd66dcf143e24d3f4e7bb12518af7b2a12ea1c2
Elien <145133444+CURT-Elien-2326035a@users.noreply.github.com>
2023-10-09 12:04:35 +0200

    Update morpionElien.cpp

--- morpionElien.cpp
+++ morpionElien.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int caseCheck ()
+{
+    int cases = 8;
+    int caseCheck = 1;
+    int inVar;
+    cout<< "entrer valeur de la case (8)";
+    cin >> inVar;
+    if(inVar==cases && caseCheck == 0){
+        cout<< "case full";
+    }
+    if(inVar==cases && caseCheck == 1){
+        cout<< "case empty";
+        caseCheck = 0;
+    }
+}
+
+int main() {
+    while (true)
+    {
+        caseCheck();
+    }
+}
 
