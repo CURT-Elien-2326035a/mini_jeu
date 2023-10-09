3726ce15fa58f70304b4558a2724c6498d92bcf4
Albano Coline <144835859+ALBANO-Coline-2326001@users.noreply.github.com>
2023-10-09 12:02:45 +0200

    debut du programme

--- morpion.cpp
+++ morpion.cpp
@@ -1,1 +1,54 @@
-//final
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool end () 
+{
+    if (victory_line)
+    {
+        return true ;
+    }
+    else if (victory_column)
+    {
+        return true ;
+    }
+    else if (victory_diagonal)
+    {
+        return true ;
+    }
+    else if (egality)
+    {
+        return true ;
+    }
+    else 
+    {
+        return false ;
+    }
+}
+
+void start ()
+{
+    vector <vector<string>> morpion {{"","",""}, {"","",""}, {"","",""}} ;
+    int count (0) ;
+    while (true)
+    {
+        count += 1 ;
+        place_form(count); //place form vérifie que la case est vide    
+    }
+    
+    char yesno ('y');
+    cout << "Si vous voulez rejouer, taper 'y', sinon tapez 'n'" << endl ;
+    if (yesno == 'y') 
+    {
+        start();
+    }
+}
+
+
+
+int main()
+{
+    start ();
+}
+
