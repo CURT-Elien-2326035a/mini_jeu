d354aac5ec8a50af692be8b53abb7c361b442a11
Bol2riz02 <146104049+Bol2riz02@users.noreply.github.com>
2023-10-16 10:55:19 +0200

    Update justeprix.cpp

--- justeprix.cpp
+++ justeprix.cpp
@@ -5,19 +5,20 @@ using namespace std;
 int main()
 {
 	int prix = 67;
-	int score = 100; 
-	unsigned tentative = 0; 
+	int score = 100;
+	unsigned tentative = 0;
 	cout << "Le prix est entre 0 et 100. Devinez le prix : ";
 	while (true)
 	{
-		unsigned x;
+		int x;
 		cin >> x;
 		tentative += 1;
+		score -= 5;
 		if (x < prix) cout << "Le prix est plus haut : ";
 		if (x > prix) cout << "Le prix est plus bas : ";
 		if (x == prix)
 		{
-			cout << "Vous avez gagne. Bravo, votre score est de : " << score / tentative << endl;
+			cout << "Vous avez gagne. Bravo, votre score est de : " << score << endl;
 			cout << "Votre total de tentatives est de : " << tentative << endl;
 			break;
 		}
